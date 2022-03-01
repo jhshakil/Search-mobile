4f74827bc1d9bebdb3dca002ff5692a48ffbe38e
Shakil <shakil11271@gmail.com>
2022-03-01 16:59:40 +0600

    position set

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const search = () => {
     const input = document.getElementById('input');
     const value = input.value;
+    input.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${value}`;
     fetch(url)
         .then(res => res.json())
@@ -13,13 +14,14 @@ const displayOutput = values => {
     values.forEach(datas => {
         // console.log(datas);
         const div = document.createElement('div');
+        div.classList.add('bg-white', 'rounded-lg')
         div.innerHTML = `
         <div class="border-solid border-gray-300 border-2 rounded-xl p-3">
                     <img class="rounded-xl block m-auto" src="${datas.image}" alt="Image">
                     <h2 class="text-center text-xl m-2 font-bold">${datas.phone_name}</h2>
                     <h2 class="text-center text-cyan-800 mt-2">Brand : <span class="text-xl font-bold">${datas.brand}</span>
                     </h2>
-                    <button onclick="detalse('${datas.slug}')" class="bg-gray-300 pl-4 pr-4 p-1 rounded block m-auto mt-4">Detalse</button>
+                    <button onclick="detalse('${datas.slug}')" class="bg-orange-300 pl-4 pr-4 p-1 rounded block m-auto mt-4">Detalse</button>
                 </div>
         `
         output.appendChild(div);
@@ -37,10 +39,12 @@ const detalse = id => {
 
 const displayDetalse = values => {
     const detalse = document.getElementById('detalse');
-    console.log(values.data.mainFeatures);
+    detalse.innerHTML = '';
+    // console.log(values.data.mainFeatures);
     const div = document.createElement('div');
+    div.classList.add('bg-orange-200', 'p-4', 'rounded-lg', 'drop-shadow-2xl');
     div.innerHTML = `
-    <div class="w-1/2 block m-auto">
+    <div class="block m-auto">
                     <h1 class="text-center text-3xl font-bold">${values.data.name}</h1>
                     <h3 class="text-center text-lg font-bold mt-2">Release Date: </h3>
                     <h3 class="text-center">${values.data.releaseDate}</h3>
@@ -55,7 +59,13 @@ const displayDetalse = values => {
                     <h3 class="text-center">${values.data.mainFeatures.memory}</h3>
                     <h3 class="text-center text-lg font-bold mt-2">Sensors</h3>
                     <h3 class="text-center">${values.data.mainFeatures.sensors}</h3>
+                    <button onclick='closing()' class="bg-orange-500 pl-8 pr-8 p-1 rounded block m-auto mt-4">Close</button>
                 </div>
     `
     detalse.appendChild(div)
+}
+
+const closing = () => {
+    const items = document.getElementById('detalse');
+    items.innerHTML = '';
 }
